74ce355fb8ee9fc3fcd21db65d1030a1b5ba112b
amarinediary <bp.brizzi@gmail.com>
2021-08-04 11:23:02 +0200

    initial commit

--- public/js/script.js
+++ public/js/script.js
@@ -89,16 +89,6 @@ $(() => {
 
             $('#loader').removeClass('d-none');
 
-                let words = ['Connexion au NetAirClub', 'Vérification du statut', 'Extraction du contenu', 'Formatage des données', 'Genèse du fichier .ics'];
-                let count = 0;
-                setInterval(() => {
-                    if (count < words.length) {
-                        let word = words[count];
-                        $('#loader-feedback').html(word);
-                        count++;   
-                    };
-                }, 3000);
-
             $('body').addClass('pe-none overflow-hidden');
 
             $.ajax({
@@ -111,7 +101,7 @@ $(() => {
                     password: password,
                 }),
                 success: (response) => {
-                    console.log(response);
+                    //console.log(response);
                     let stamp = Date.now();
                     if (response.status === 404) {
                         $('.toast-container').append(`<div id="toast-${stamp}" class="toast align-items-center text-white bg-danger border-0 mb-3 hide" style="max-width:100%;"><div class="d-flex"><div class="toast-body text-truncate">Vôtre <a href="${response.url}" target="_blank" rel="noopener">NetAirClub</a> ne répond pas, erreur ${response.status}.</div><button type="button" class="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close"></button></div></div>`);
